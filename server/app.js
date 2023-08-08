30c22caf65b8b781e5d501cfcfa7b1476058bd68
Juan Buitrago <buitr4go@gmail.com>
2023-08-08 01:29:45 -0700

    v1.0 cors fix

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "https://film-folia.vercel.app",
+    origin: "http://localhost:5173",
     credentials: true,
 }));
 app.use(bodyParser.urlencoded({ extended: true }));
